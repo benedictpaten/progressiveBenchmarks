b55f77c7eb08593ad8440f3add63807b445abf3d
Benedict <benedictpaten@gmail.com>
2013-05-07 11:04:26 -0700

    -

--- src/paramsGenerator.py
+++ src/paramsGenerator.py
@@ -127,7 +127,7 @@ class KyotoTycoon(ParamsGenerator):
     def __init__(self):
         ParamsGenerator.__init__(self)
         self.kyotoTycoon = [ "inMemory" ] #[True, "inMemory", "inMemoryNoSnapshot"]
-        self.outgroupStrategy = ['greedyLeaves' ]
+        self.outgroupStrategy = ['greedy' ]
         self.outgroupThreshold = [0]
         self.selfAlignment = [False]
         self.vanilla = [False]
