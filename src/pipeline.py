2e77b12fc51fcf307b8fc524ed950e5df1b24a60
benedictpaten <benedictpaten@gmail.com>
2013-10-03 21:03:10 -0700

    -

--- src/pipeline.py
+++ src/pipeline.py
@@ -593,7 +593,7 @@ class Make3Turtles(MakeEvolverPrimatesLoci1):
     name = "threeTurtles"
     def run(self):
         simDir = os.path.join(TestStatus.getPathToDataSets(), "realReptiles")
-        sequences, newickTreeString = getInputs(simDir, ("cb4.fa", "caeRem4.fa", "caePb3.fa"))
+        sequences, newickTreeString = getInputs(simDir, ("cheMyd1.fa", "chrPic1.fa", "pelSin1.fa"))
         newickTreeString = "((cheMyd1:0.1,chrPic1:0.1)Anc0:1.0,pelSin1:0.1)MRCA;" # "((HUMAN:0.144018,MOUSE:0.356483)Anc0:0.0238,DOG:0.197)MRCA;" #Over-ride the full phylogeny
         outputDir = os.path.join(self.options.outputDir, "%s%s"  % (self.name, self.params))
         self.addChildTarget(MakeAlignment(self.options, sequences, newickTreeString, outputDir,
